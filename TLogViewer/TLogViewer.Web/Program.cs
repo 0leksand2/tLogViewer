99c48f191cef5590d6226c8eee7215a63570dd5b
Oleksandr Demianenko <ale.de635@gmail.com>
2026-07-17 19:52:53 +0300

    add client for web app

--- TLogViewer/TLogViewer.Web/Program.cs
+++ TLogViewer/TLogViewer.Web/Program.cs
@@ -3,21 +3,32 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularDev", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
+app.UseDefaultFiles();   
+app.UseStaticFiles();    
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseCors("AllowAngularDev");
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
